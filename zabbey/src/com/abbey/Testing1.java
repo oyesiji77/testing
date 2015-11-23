e18eae45024521ae824dbfe7378722d5a8e6f2d0
JayBeen <oyesiji@gmail.com>
2015-11-23 16:12:08 -0500

    made changes

--- zabbey/src/com/abbey/Testing1.java
+++ zabbey/src/com/abbey/Testing1.java
@@ -12,20 +12,7 @@ package com.abbey;
 public class Testing1 {
 
   public static void main(String[] args) {
-    System.out.println("###its working now########");
-    System.out.println("###its working 66########");
-    System.out.println("###its working 33########");
-    System.out.println("###its working 11########");
-     System.out.println("###its working 1d1########");
-     System.out.println("###its working ssa########");
-System.out.println("###dodo########");
-
-System.out.println("###dodo  44########");
-System.out.println("###dodo  55########");
-
-System.out.println("###Ibadan########");
-System.out.println("###dodo  66########");
-System.out.println("###Lago########");
+	  System.out.println("Work1");
   }
 
 }
